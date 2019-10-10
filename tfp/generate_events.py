f70e47706452deb4b699a6f7b8852b2788976bb3
Joshua Isaacson <isaacson@fnal.gov>
2019-10-09 20:59:47 -0500

    Adjust generate_events to loop over mass and width internally

--- tfp/generate_events.py
+++ tfp/generate_events.py
@@ -1,6 +1,7 @@
 """ Generate events using pythia8. """
 
 import argparse
+import numpy as np
 
 import pythia8
 
@@ -10,24 +11,42 @@ def parse_input():
                                      'production for a given mass and width')
     parser.add_argument('--nevents', '-n', type=int, default=1000,
                         help='Number of events to generate')
-    parser.add_argument('--mass', '-m', type=float, default=80.,
-                        help='Mass of W-boson')
-    parser.add_argument('--width', '-w', type=float, default=2.,
-                        help='Width of W-boson')
-    parser.add_argument('--filename', '-f', type=str, default='events.txt',
-                        help='Name of output file')
+    parser.add_argument('--mass_min', type=float, default=70.,
+                        help='Minimum mass for W-boson')
+    parser.add_argument('--mass_max', type=float, default=90.,
+                        help='Maximum mass for W-boson')
+    parser.add_argument('--mass_step', type=float, default=2.,
+                        help='Mass step for W-boson')
+    parser.add_argument('--width_min', type=float, default=0.8,
+                        help='Minimum width for W-boson')
+    parser.add_argument('--width_max', type=float, default=2.8,
+                        help='Maximum width for W-boson')
+    parser.add_argument('--width_step', type=float, default=0.2,
+                        help='Width step for W-boson')
 
     return parser.parse_args()
 
-def main():
-    """ Run pythia """
-    args = parse_input()
 
+def main():
+    """ Loop over all combinations of mass and width. """
+    args = parse_input()
+    masses = np.linspace(args.mass_min, args.mass_max,
+                         np.ceil((args.mass_max - args.mass_min) / args.mass_step)+1)
+    widths = np.linspace(args.width_min, args.width_max,
+                         np.ceil((args.width_max - args.width_min) / args.width_step)+1)
+
+    for mass in masses:
+        for width in widths:
+            run(args.nevents, mass, width)
+
+
+def run(nevents, mass, width):
+    """ Run pythia """
     pythia = pythia8.Pythia()
     pythia.readString('Beams:eCM = 14000.')
     pythia.readString('WeakSingleBoson:ffbar2W = on')
-    pythia.readString('24:m0 = {}'.format(args.mass))
-    pythia.readString('24:mWidth = {}'.format(args.width))
+    pythia.readString('24:m0 = {}'.format(mass))
+    pythia.readString('24:mWidth = {}'.format(width))
     pythia.readString('24:onMode = off')
     pythia.readString('24:onIfMatch = 1 2')
     pythia.readString('24:onIfMatch = 1 4')
@@ -41,15 +60,16 @@ def main():
     pythia.readString('Check:event = off')
     pythia.init()
 
-    with open(args.filename, 'w') as outfile:
-        for _ in range(args.nevents):
+    filename = 'events_{:.1f}_{:.1f}.txt'.format(mass, width)
+    with open(filename, 'w') as outfile:
+        for _ in range(nevents):
             if not pythia.next():
                 continue
 
             for particle in pythia.event:
                 if particle.isFinal():
                     outfile.write(
-                        '[{: 8e}, {: 8e}, {: 8e}, {: 8e}] '.format(
+                        '[{: .8e}, {: .8e}, {: .8e}, {: .8e}] '.format(
                             particle.px(), particle.py(), particle.pz(), particle.e()))
 
             outfile.write('\n')
