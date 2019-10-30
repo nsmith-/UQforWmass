dfc7f1c711ef5fbfcb86d55135d9cb65b56f0410
Nick Smith <nick.smith@cern.ch>
2019-10-30 13:39:01 -0500

    Make pythia (optionally) shut up

--- tfp/generate_events.py
+++ tfp/generate_events.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
 """ Generate events using pythia8. """
 
 import argparse
 import numpy as np
+import tqdm
 
 import pythia8
 
@@ -10,19 +12,21 @@ def parse_input():
     parser = argparse.ArgumentParser(description='Generate nevents for W '
                                      'production for a given mass and width')
     parser.add_argument('--nevents', '-n', type=int, default=1000,
-                        help='Number of events to generate')
+                        help='Number of events to generate (default: %(default)r)')
     parser.add_argument('--mass_min', type=float, default=70.,
-                        help='Minimum mass for W-boson')
+                        help='Minimum mass for W-boson (default: %(default)r)')
     parser.add_argument('--mass_max', type=float, default=90.,
-                        help='Maximum mass for W-boson')
+                        help='Maximum mass for W-boson (default: %(default)r)')
     parser.add_argument('--mass_step', type=float, default=2.,
-                        help='Mass step for W-boson')
+                        help='Mass step for W-boson (default: %(default)r)')
     parser.add_argument('--width_min', type=float, default=0.8,
-                        help='Minimum width for W-boson')
+                        help='Minimum width for W-boson (default: %(default)r)')
     parser.add_argument('--width_max', type=float, default=2.8,
-                        help='Maximum width for W-boson')
+                        help='Maximum width for W-boson (default: %(default)r)')
     parser.add_argument('--width_step', type=float, default=0.2,
-                        help='Width step for W-boson')
+                        help='Width step for W-boson (default: %(default)r)')
+    parser.add_argument('--verbose', '-v', type=bool, default=False,
+                        help='Let Pythia print verbose info (default: %(default)r)')
 
     return parser.parse_args()
 
@@ -35,14 +39,25 @@ def main():
     widths = np.linspace(args.width_min, args.width_max,
                          np.ceil((args.width_max - args.width_min) / args.width_step)+1)
 
-    for mass in masses:
-        for width in widths:
-            run(args.nevents, mass, width)
+    with tqdm.tqdm(unit='event', total=masses.size * widths.size * args.nevents, desc='Generating') as pbar:
+        for mass in masses:
+            for width in widths:
+                run(args.nevents, mass, width)
+                pbar.update(args.nevents)
 
 
-def run(nevents, mass, width):
+def run(nevents, mass, width, debug=False):
     """ Run pythia """
-    pythia = pythia8.Pythia()
+    pythia = pythia8.Pythia('', debug)
+    if not debug:
+        pythia.readString('Init:showProcesses = off')
+        pythia.readString('Init:showMultipartonInteractions = off')
+        pythia.readString('Init:showChangedSettings = off')
+        pythia.readString('Init:showChangedParticleData = off')
+        pythia.readString('Next:numberCount = 0')
+        pythia.readString('Next:numberShowInfo = 0')
+        pythia.readString('Next:numberShowProcess = 0')
+        pythia.readString('Next:numberShowEvent = 0')
     pythia.readString('Beams:eCM = 14000.')
     pythia.readString('WeakSingleBoson:ffbar2W = on')
     pythia.readString('24:m0 = {}'.format(mass))
@@ -75,7 +90,8 @@ def run(nevents, mass, width):
 
             outfile.write('\n')
 
-    pythia.stat()
+    if debug:
+        pythia.stat()
 
 
 if __name__ == '__main__':
